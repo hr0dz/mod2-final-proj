344f5f62dfd65a5b9fbfbe366964ddf9edbac624
doodleman218 <dcwolff218@gmail.com>
2020-05-05 12:00:28 -0500

    added a business index/show page

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,6 +1,17 @@
 class NeighborhoodsController < ApplicationController
 
-    private
+    
+  def index
+    @neighborhoods = Neighborhood.all
+  end
+  
+  def show
+    @neighborhood = Neighborhood.find(params[:id])
+  end
+  
+  
+  
+  private
     
     def neighborhood_params
         params.require(:neighborhood).permit(
