c4ad6e6a38d3ec160273e75235856da59b940a94
doodleman218 <dcwolff218@gmail.com>
2020-05-06 19:02:06 -0500

    some css stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do 
   resources :notes, only: [:index, :show, :edit, :update]
-  #resources :notes, only: [:index, :show]
   resources :users, only: [:new, :create, :show, :index, :edit, :update]
   resources :neighborhoods, only: [:index, :show]
   resources :businesses, only: [:index, :show] do
   resources :notes, only: [:new, :create]
-
-  # get '/businesses/:business_id/notes/new', to: 'notes#new', as: 'new_note'
-  # post '/businesses/:business_id/notes', to: 'notes#create'
-  # post '/notes', to: 'notes#create'
 end 
+  root("neighborhoods#index")
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
