8595d5a41df23b102ccdd91affb2b6282deabd67
github username <github email address>
2020-05-08 14:26:37 -0500

    donezo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-    #def set_current_user
-       # User.current = current_user
-     #end
-
-      #def current_user?(obj)
-       # current_user == obj.user 
-      #end
+ 
 end
